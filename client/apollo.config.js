eed5b872a0604e0551b6cab99011ab710d78ffa2
Nils Littmann <n.littmann@finetunes.net>
2019-06-19 12:54:56 +0200

    Cleanup of apollo.config.js

--- client/apollo.config.js
+++ client/apollo.config.js
@@ -3,9 +3,10 @@ module.exports = {
     service: {
       name: 'graphql-product',
       url: 'http://localhost:8080/graphql',
-      headers: { "Grass-Account-Type": "vendor",
-      "Grass-Account-Id": 15554 }
-      // url: 'https://m3507x64l8.sse.codesandbox.io/',
+      headers: { 
+        "Grass-Account-Type": "vendor",
+        "Grass-Account-Id": 15554 
+      },
     },
   },
 };
